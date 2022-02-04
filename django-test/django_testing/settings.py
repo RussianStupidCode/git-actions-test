1aca1c6bc6a93c2e2474d0eec93f7f89075c1dff
artur <i@aderbin.ru>
2022-02-04 21:32:22 +0500

    fix procfile heroku

--- django-test/django_testing/settings.py
+++ django-test/django_testing/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 
 if os.getenv("HEROKU_DEPLOY"):
     import dj_database_url
-    DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600, ssl_require=True)
+    DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
 
 
 # Password validation
