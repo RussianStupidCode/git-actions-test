a6eef538f8ba999c2724b196ed1c2866fa0f29c3
artur <i@aderbin.ru>
2022-02-04 21:02:15 +0500

    fix settings

--- django-test/django_testing/settings.py
+++ django-test/django_testing/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
-if os.getenv("HEROKU_DEPLOY"):
-    import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,5 +135,5 @@ MAX_STUDENTS_PER_COURSE = 20
 
 STATIC_URL = '/static/'
 
-if os.getenv("HEROKU_DEPLOY"):
-    django_heroku.settings(locals())
+
+django_heroku.settings(locals())
